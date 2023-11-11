fc9afdf1bb4fabbae810e907051426f0970b1c8e
sunnxe <s778906@gmail.com>
2023-11-11 17:24:26 +1100

    audio included & pic to delle3

--- src/pages/storygenerated.jsx
+++ src/pages/storygenerated.jsx
@@ -1,5 +1,8 @@
 import { useLocation } from 'react-router-dom';
 import LoadingAnimation from '../assets/img/nap.gif';
+import React, { useState } from 'react';
+import Speech from '../speech.mp3';
+
 
 export default function Example() {
   const location = useLocation();
@@ -23,6 +26,12 @@ export default function Example() {
     { label: '日期', value: today },
   ];
 
+  // Function to play the audio
+    const playAudio = () => {
+      const audio = new Audio(Speech); // Adjust the path as necessary
+      audio.play().catch(error => console.error('Error playing the audio', error));
+    };
+
   if (!story) {
     return (
       <div className="flex justify-center items-center mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 h-screen">
@@ -39,7 +48,7 @@ export default function Example() {
             <div className="relative overflow-hidden rounded-3xl bg-gray-900 px-6 pb-9 pt-64 shadow-2xl sm:px-12 lg:max-w-lg lg:px-8 lg:pb-8 xl:px-10 xl:pb-10">
               <img
                 className="absolute inset-0 h-full w-full object-cover "
-                src={imageUrl.data[0].url}
+                src={imageUrl}
                 alt="Story Picture"
               />
               {/* <div className="absolute inset-0 bg-gray-900 mix-blend-multiply" /> */}
@@ -93,9 +102,11 @@ export default function Example() {
                   {title}
                 </h1>
 
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
-                </svg>
+                <button onClick={playAudio} className="focus:outline-none">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M10.34 15.84c-.688-.06-1.386-.09-2.09-.09H7.5a4.5 4.5 0 110-9h.75c.704 0 1.402-.03 2.09-.09m0 9.18c.253.962.584 1.892.985 2.783.247.55.06 1.21-.463 1.511l-.657.38c-.551.318-1.26.117-1.527-.461a20.845 20.845 0 01-1.44-4.282m3.102.069a18.03 18.03 0 01-.59-4.59c0-1.586.205-3.124.59-4.59m0 9.18a23.848 23.848 0 018.835 2.535M10.34 6.66a23.847 23.847 0 008.835-2.535m0 0A23.74 23.74 0 0018.795 3m.38 1.125a23.91 23.91 0 011.014 5.395m-1.014 8.855c-.118.38-.245.754-.38 1.125m.38-1.125a23.91 23.91 0 001.014-5.395m0-3.46c.495.413.811 1.035.811 1.73 0 .695-.316 1.317-.811 1.73m0-3.46a24.347 24.347 0 010 3.46" />
+                  </svg>
+                </button>
               </div>
 
 
--- src/server.js
+++ src/server.js
@@ -1,74 +1,77 @@
 const express = require('express');
 const cors = require('cors');
-const fetch = require('node-fetch'); // make sure to install node-fetch if not already installed
-
-const openai = require('openai');
-const { Configuration, OpenAIApi } = openai;
-
+require('dotenv').config();
+// Initialize OpenAI SDK
+const { OpenAI } = require('openai');
+const path = require('path');
+const fs = require('fs');
 
 const app = express();
-
-// Use cors and body-parser middleware
 app.use(cors());
 app.use(express.json());
 
-const OPENAI_API_KEY='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';
 
-// Combined endpoint to process story and image
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const speechFile = path.resolve("./speech.mp3");
+
+
 app.post('/process-story-and-image', async (req, res) => {
-  console.log('Received request for /process-story-and-image');
-  const { selectedAnimal, selectedColor, challenge, selectedSkill } = req.body;
+  console.log("Get the post request")
+    try {
+        const { selectedAnimal, selectedColor, challenge, selectedSkill } = req.body;
 
-  // Story generation options
-  const storyOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      model: "gpt-4-1106-preview",
-      messages: [
-        { role: 'system', content: 'You are a great story teller. Please include a title in your story. It should have 標題: 故事開始:' },
-        { role: 'user', content: `Create a story with a ${selectedAnimal}, color ${selectedColor}, challenge ${challenge}, and skill ${selectedSkill} for a child around 2 to 3 years old in traditional chinese. The story should start with the title and be followed by the story.` },
-      ],
-    }),
-  };
+        // Validate input
+        if (!selectedAnimal || !selectedColor || !challenge || !selectedSkill) {
+            return res.status(400).send('Missing required fields');
+        }
 
-  // Image generation options
-  const imageOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'Authorization': `Bearer ${OPENAI_API_KEY}`
-    },
-    body: JSON.stringify({
-      prompt: `Cute Story Illustration of ${selectedAnimal} in ${selectedColor} having a challenge of ${challenge} seek for the skill of ${selectedSkill} for the age of 2 to 3 years old child to visualize.`,
-      n:1,
-    }),
-  };
+      // Story generation using OpenAI SDK - using a chat model
+      const storyResponse = await openai.chat.completions.create({
+        model: "gpt-4-1106-preview",
+        messages: [
+            { role: 'system', content: 'You are a great story teller. Please include a title in your story. It should have 標題: 故事開始:' },
+            { role: 'user', content: `Create a story with a ${selectedAnimal}, color ${selectedColor}, challenge ${challenge}, and skill ${selectedSkill} for a child around 2 to 3 years old in traditional Chinese. The story should start with the title and be followed by the story.` },
+        ],
+      });
 
-  try {
-    // Generate story
-    const storyResponse = await fetch('https://api.openai.com/v1/chat/completions', storyOptions);
-    const storyData = await storyResponse.json();
+        // Image generation using OpenAI SDK
+        const imageResponse = await openai.images.generate({
+            prompt: `Illustration a charming ${selectedAnimal} in ${selectedColor}. This animal is facing a whimsical challenge of ${challenge} and is seeking to learn or use the skill of ${selectedSkill}. The scene should be simple yet colorful, suitable for children aged 2 to 3 years old. The style should be cartoonish with exaggerated features for clear emotional and action cues. Please infuse the illustration with dreamy and magical elements, like sparkling stars, colorful bubbles, or a mystical backdrop. Ensure there are no words in the illustration.`,
+            n: 1,
+            size: "1792x1024",
+            model: "dall-e-3",
+        });
 
-    // Generate image
-    const imageResponse = await fetch('https://api.openai.com/v1/images/generations', imageOptions);
-    const imageData = await imageResponse.json(); // get the JSON response
+        // Extract the story text
+        const storyText = storyResponse.choices[0].message.content;
 
-    console.log('Image Response:', imageData); // log the entire response
+        // Generate audio from the story text
+        const mp3Response = await openai.audio.speech.create({
+            model: "tts-1",
+            voice: "nova",
+            input: storyText,
+            speed: "1",
+        });
 
+        console.log(speechFile);
+        const buffer = Buffer.from(await mp3Response.arrayBuffer());
+        await fs.promises.writeFile(speechFile, buffer);
 
-    // Send back the story data and the image URL
-    res.send({ story: storyData.choices[0].message.content, imageUrl: imageData });
-  } catch (error) {
-    console.error("An error occurred:", error);
-    res.status(500).send("An error occurred while processing the request.");
-  }
+        // Send back the story data and the image URL
+        res.send({ 
+            story: storyResponse.choices[0].message.content, 
+            imageUrl: imageResponse.data[0].url // Adjust this based on the structure of the response
+        });
+    } catch (error) {
+        console.error("An error occurred:", error);
+        res.status(500).send("An error occurred while processing the request.");
+    }
 });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
